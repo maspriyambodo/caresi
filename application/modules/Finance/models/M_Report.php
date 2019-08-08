62f5fe78e1fdb516ec405728aaef71fb2b6516b0
maspriyambodo <maspriyambodo@gmail.com>
2019-08-08 21:07:51 +0700

    edited finance module

--- application/modules/Finance/models/M_Report.php
+++ application/modules/Finance/models/M_Report.php
@@ -15,14 +15,14 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class M_Report extends CI_Model {
 
     function index($tahun) {
-        $exec = $this->db->select('ppu.no_ppu,ppu.tgl_pembayaran,ppu.vendor,proyek.nama_proyek,ppu.jumlah,ppu.harga,(SELECT SUM(ppu.jumlah * ppu.harga) FROM ppu WHERE ppu.no_ppu=proyek.id_ppu) AS total ,( SELECT SUM( jumlah * harga ) FROM ppu WHERE ppu.stat = 2 ) AS gtotal ')
+        $exec = $this->db->select('ppu.no_ppu,ppu.tgl_pembayaran,ppu.vendor,proyek.nama_proyek,ppu.jumlah,ppu.harga,(SELECT SUM(ppu.jumlah * ppu.harga) FROM ppu WHERE ppu.stat = 2 AND ppu.no_ppu=proyek.id_ppu) AS total ,( SELECT SUM( jumlah * harga ) FROM ppu WHERE ppu.stat = 2 AND YEAR ( ppu.tgl_pembayaran ) = ' . $tahun . ' ) AS gtotal ')
                 ->from('proyek')
                 ->join('ppu', 'proyek.id_ppu = ppu.no_ppu', 'LEFT')
-                ->where(['ppu.stat' => 2, 'YEAR ( ppu.tgl_pembayaran ) =' => date("Y")])
+                ->where(['ppu.stat' => 2, 'YEAR ( ppu.tgl_pembayaran ) =' => $tahun])
                 ->group_by('ppu.no_ppu')
                 ->get()
                 ->result();
-//        print_r($this->db->last_query());die;
+        //print_r($this->db->last_query());die;
         return $exec;
     }
 
