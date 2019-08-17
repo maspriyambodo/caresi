97cbf0ad088cb89b3af4672b977736877cdeb5af
maspriyambodo <maspriyambodo@gmail.com>
2019-08-17 08:11:13 +0700

    Update v_addppu.php

--- application/modules/Admin/views/v_addppu.php
+++ application/modules/Admin/views/v_addppu.php
@@ -19,7 +19,7 @@
             <div class="form-group">
                 <label class="text-uppercase">tmt start :</label>
                 <div class="input-group" id="tmtstart">
-                    <input type="text" class="form-control" name="tmtstart" required="" autocomplete="off">
+                    <input type="text" class="form-control" name="tmtstart" required="" autocomplete="off" readonly="true">
                     <span class="input-group-addon" id="basic-addon2"><i class="glyphicon glyphicon-calendar"></i></span>
                 </div>
             </div>
@@ -32,7 +32,7 @@
             <div class="form-group">
                 <label class="text-uppercase">tmt stop :</label>
                 <div class="input-group" id="tmtstop">
-                    <input type="text" class="form-control" name="tmtstop" required="" autocomplete="off">
+                    <input type="text" class="form-control" name="tmtstop" required="" autocomplete="off" readonly="true">
                     <span class="input-group-addon"><i class="glyphicon glyphicon-calendar"></i></span>
                 </div>
             </div>
@@ -49,7 +49,7 @@
         <div class="col-md-3">
             <div class="form-group" style="margin:0px;">
                 <label class="text-uppercase">jumlah :</label>
-                <input type="text" name="jumlah[]" class="form-control" required="" autocomplete="off">
+                <input type="text" name="jumlah[]" class="form-control" required="" onkeypress="return isNumberKey(event)" autocomplete="off">
             </div>
         </div>
         <div class="col-md-3">
@@ -61,7 +61,7 @@
         <div class="col-md-3">
             <div class="form-group" style="margin:0px;">
                 <label class="text-uppercase">harga :</label>
-                <input type="text" name="harga[]" class="form-control" required="" autocomplete="off">
+                <input type="text" name="harga[]" onkeypress="return isNumberKey(event)" class="form-control" required="" autocomplete="off">
             </div>
         </div>
     </div>
@@ -105,14 +105,20 @@
             toggleActive: true
         });
     };
+    function isNumberKey(evt) {
+        var charCode = (evt.which) ? evt.which : event.keyCode
+        if (charCode > 31 && (charCode < 48 || charCode > 57))
+            return false;
+        return true;
+    }
     function additem() {
         $("<div style='clear:both;margin:10px 0px;'></div>").appendTo('#keterangan');
         $("<input type='text' class='form-control' name='keterangan[]' required='' autocomplete='off'>").appendTo('#keterangan');
         $("<div style='clear:both;margin:10px 0px;'></div>").appendTo('#jumlah');
-        $("<input type='text' class='form-control' name='jumlah[]' required='' autocomplete='off'>").appendTo('#jumlah');
+        $("<input type='text' class='form-control' name='jumlah[]' required='' onkeypress='return isNumberKey(event)' autocomplete='off'>").appendTo('#jumlah');
         $("<div style='clear:both;margin:10px 0px;'></div>").appendTo('#satuan');
         $("<input type='text' class='form-control' name='satuan[]' required='' autocomplete='off'>").appendTo('#satuan');
         $("<div style='clear:both;margin:10px 0px;'></div>").appendTo('#harga');
-        $("<input type='text' class='form-control' name='harga[]' required='' autocomplete='off'>").appendTo('#harga');
+        $("<input type='text' class='form-control' name='harga[]' required='' autocomplete='off' onkeypress='return isNumberKey(event)'>").appendTo('#harga');
     }
 </script>
